4857764aa597246a6d11112b07f1db3bdbc84abb
DESKTOP\iandidriksen <iandidri@gmail.com>
2018-09-03 17:17:06 -0300

    Se comenzo a implementar Unit y Player. Cambios al UML respecto de lo que nos indicaron.

--- src/Unit/Player.java
+++ src/Unit/Player.java
@@ -1,5 +1,132 @@
 package Unit;
 
 public class Player extends Unit {
-
+	private static final int maxLife = 100;
+	
+	//Attributes
+	private int shield;
+	private int score;
+	
+	private int dx;
+	private int dy;
+	private int speed;
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	//Constructor
+	public Player(int cX, int cY) {
+		life = maxLife;
+		shield = 0;
+		score = 0;
+		
+		coordX = cX;
+		coordY = cY;
+		dx = dy = 0;
+		speed = 5;
+		left = right = up = down = false;		
+	}
+	
+	
+	//Commands
+	public void setSield() {
+		shield = 100;
+	}
+	
+	
+	@Override
+	/**
+	 * Redefine el metodo setDamage() de Unit.
+	 * @return Boolean si el personaje esta muerto luego de recibir daño,
+	 * falso caso contrario.
+	 */
+	public boolean setDamage(int damage) {
+		if(shield > 0) {
+			 int diff = damage - shield;
+			 if (diff > 0) {
+				 shield = 0;
+				 return super.setDamage(damage - shield);
+			 } else {
+				 shield -= damage;
+				 return false;
+			 }
+		}
+		return super.setDamage(damage);
+	}
+	
+	
+	/**
+	 * Cura todos los puntos de vida del personaje.
+	 */
+	public void setPotion() {
+		life = maxLife;
+	}
+	
+	
+	/**
+	 * Retorna un entero conteniendo el score del jugador.
+	 * @return Entero con el score de jugador.
+	 */
+	public int getScore() {
+		return score;		
+	}
+	
+	
+	/**
+	 * Setea el movimiento hacia la izquierda de acuerdo a un booleano
+	 * pasado como parametro.
+	 * @param Booleano que determina el movimiento hacia la izquierda.
+	 */
+	public void setLeft(boolean b) {
+		left = b;
+	}
+	
+	
+	/**
+	 * Setea el movimiento hacia la derecha de acuerdo a un booleano
+	 * pasado como parametro.
+	 * @param Booleano que determina el movimiento hacia la derecha.
+	 */
+	public void setRight(boolean b) {
+		right = b;
+	}
+	
+	
+	/**
+	 * Setea el movimiento hacia arriba de acuerdo a un booleano
+	 * pasado como parametro.
+	 * @param Booleano que determina el movimiento hacia arriba.
+	 */
+	public void setUp(boolean b) {
+		up = b;
+	}
+	
+	
+	/**
+	 * Setea el movimiento hacia abajo de acuerdo a un booleano
+	 * pasado como parametro.
+	 * @param Booleano que determina el movimiento hacia abajo.
+	 */
+	public void setDown(boolean b) {
+		down = b;
+	}
+	
+	
+	/**
+	 * Añade la cantidad de puntos pasada como parametro al score del jugador.
+	 * @param Entero s con los puntos a añadir.
+	 */
+	public void addScore(int s) {
+		score += s;
+	}
+	
+	
+	/**
+	 * Actualiza al jugador de acuerdo a los atributos.
+	 */
+	public void update() {
+		
+	}
+	
 }
--- src/Unit/Unit.java
+++ src/Unit/Unit.java
@@ -1,5 +1,51 @@
 package Unit;
 
 public abstract class Unit {
-
+	//Attributes
+	protected int life;
+	protected int coordX; 
+	protected int coordY;
+	
+	//Commands
+	/**
+	 * Consulta que retorna un entero con la posicion X de la unidad.
+	 * @return Entero con la posicion X de la unidad.
+	 */
+	public int getX() {
+		return coordX;
+	}
+	
+	/**
+	 * Consulta que retorna un entero con la posicion Y de la unidad.
+	 * @return Entero con la posicion Y de la unidad.
+	 */
+	public int getY() {
+		return coordY;		
+	}
+	
+	/**
+	 * Consulta que retorna un entero con la vida restante de la unidad.
+	 * @return Entero con la vida restante de la unidad.
+	 */
+	public int getLife() {
+		return life;		
+	}
+	
+	// Command
+	/**
+	 * Quita una cantidad de vida pasada como un parametro entero a la unidad
+	 * Y retorna verdadero si la unidad esta muerta luego de recibir daño,
+	 * y falso en caso contrario.
+	 * @param Entero 'damage' indicando la cantidad de vida que pierde la unidad.
+	 * @return Boolean indicando si la unidad esta muerta o no.
+	 */
+	public boolean setDamage(int damage) {
+		if (damage >= 0) {
+			life -= damage; 
+		}
+		return (life <= 0) ? true : false;
+	}
+	
+	
+	
 }
